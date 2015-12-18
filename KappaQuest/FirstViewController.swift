cdd8731a85e547b6644ca8bbf90a1f8bd08f9a09
伊藤直哉 <n_itou@itounaoya-no-macbook-air.local>
2015-12-18 23:00:55 +0900

    Initial Commit

---
+++ KappaQuest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  KappaQuest
+//
+//  Created by 伊藤直哉 on 2015/12/18.
+//  Copyright © 2015年 ria10. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
